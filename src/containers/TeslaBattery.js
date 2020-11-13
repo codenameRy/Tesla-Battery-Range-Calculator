10b7f285240d7b4382aad4f928b40b64d8da7023
Ryan Lee <ryanm.lee11@gmail.com>
2020-11-13 01:24:41 -0500

    Tesla Notice component

--- src/containers/TeslaBattery.js
+++ src/containers/TeslaBattery.js
@@ -1,1 +1,16 @@
-rcc
+import React, { Component } from 'react';
+import './TeslaBattery.css';
+import TeslaNotice from '../components/TeslaNotice/TeslaNotice'
+
+class TeslaBattery extends Component {
+    render() {
+        return (
+            <form>
+            <h1>Range Per Charge</h1>
+            <TeslaNotice />
+            </form>
+        );
+    }
+}
+
+export default TeslaBattery;
