8db49bafc794ea87690207913eb535b226f2ffa0
Ryan Lee <ryanm.lee11@gmail.com>
2020-11-13 12:41:26 -0500

    Tesla Car stats component with props update

--- src/components/TeslaCar/TeslaCar.js
+++ src/components/TeslaCar/TeslaCar.js
@@ -7,7 +7,7 @@ const TeslaCar = (props) => (
     <div className="tesla-car">
         <div className="tesla-wheels">
             <div className={`tesla-wheel tesla-wheel--front tesla-wheel--${props.wheelsize}`}></div>
-            <div className={`tesla-wheel tesla-wheel--rear tesla-wheel--${props.wheelsize}`}>></div>
+            <div className={`tesla-wheel tesla-wheel--rear tesla-wheel--${props.wheelsize}`}></div>
         </div>
     </div>
 );
--- src/containers/TeslaBattery.js
+++ src/containers/TeslaBattery.js
@@ -2,13 +2,29 @@ import React, { Component } from 'react';
 import './TeslaBattery.css';
 import TeslaNotice from '../components/TeslaNotice/TeslaNotice';
 import TeslaCar from '../components/TeslaCar/TeslaCar';
+import TeslaStats from '../components/TeslaStats/TeslaStats'
 
 class TeslaBattery extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            carstats: [],
+            config: {
+                speed: 55,
+                temperature: 20,
+                climate: true,
+                wheels: 19
+            }
+        }
+    }
+
     render() {
+        const { config, carstats } = this.state;
         return (
-            <form>
+            <form className="tesla-battery">
             <h1>Range Per Charge</h1>
-            <TeslaCar />
+            <TeslaCar wheelsize={config.wheels}/>
+            <TeslaStats carstats={carstats}/>
             <TeslaNotice />
             </form>
         )
