20bed77da464ae9420609f6ee8507ebf53b7b305
Ryan Lee <ryanm.lee11@gmail.com>
2020-12-08 22:44:26 -0500

    Tesla Climate update

--- src/components/TeslaClimate/TeslaClimate.js
+++ src/components/TeslaClimate/TeslaClimate.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import PropTypes from 'react';
 import './TeslaClimate.css';
 
 const TeslaClimate = (props) => (
-  <div className = "tesla-climate">
+  <div className="tesla-climate">
     <label
-      className = {'tesla-climate__item ${props.value ? 'tesla-climate__item--active' : '' }  ${!props.limit ? 'tesla-heat':''}'}
+      className={`tesla-climate__item ${props.value ? 'tesla-climate__item--active' : '' }  ${!props.limit ? 'tesla-heat':''}`}
     >
       <p>{props.limit ? 'ac' : 'heat'} {props.value ? 'on' : 'off'}</p>
-      <i className = "tesla-climate__icon"></i>
+      <i className="tesla-climate__icon"></i>
       <input
-        type = "checkbox"
-        name = "climate"
-        checked = {props.value}
-        onChange = {() => {props.handleChangeClimate()}}
+        type="checkbox"
+        name="climate"
+        checked={props.value}
+        onChange={() => {props.handleChangeClimate()}}
       />
     </label>
   </div>
 );
-TeslaClimate.propTypes = {
-    value: PropTypes.bool,
-    limit: PropTypes.bool,
-    handleChangeClimate: PropTypes.func
-  }
 
-export default TeslaClimate
+TeslaClimate.propTypes = {
+  value: React.PropTypes.bool,
+  limit: React.PropTypes.bool,
+  handleChangeClimate: React.PropTypes.func
+}
+
+export default TeslaClimate;
